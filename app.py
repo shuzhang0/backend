2cbb2d6a3a5099578ba529591433752e512ea7e8
allwings <weeeallwings@outlook.com>
2022-07-09 10:38:42 +0800

    initial2

--- app.py
+++ app.py
@@ -18,12 +18,12 @@ def login():
     if request.method == 'POST':
         print("post")
         # user = request.form['username']
-        return jsonify({'status': '0', 'errmsg': 'post登录成功！'})
+        return jsonify({'status': '0', 'errmsg': 'post'})
     else:
         print("get")
         # user = request.args.get('username')  # GET方法获取数据，args是包含表单参数对及其对应值对的列表的字典对象。
-        return jsonify({'status': '0', 'errmsg': 'get登录成功！'})
+        return jsonify({'status': '0', 'errmsg': 'get'})
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, deBug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
